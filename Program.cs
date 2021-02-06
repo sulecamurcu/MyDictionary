d060ae4d0902372451ae46df9a56eec461a51207
sulecamurcu <61314662+sulecamurcu@users.noreply.github.com>
2021-02-06 19:13:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary_DersDort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(134, "Sule Camurcu");
+            Ogrenci.Add(158, "Burak Ozcelik");
+            Ogrenci.Add(115, "Rümeysa Kabaoğlu");
+            Ogrenci.Add(174, "Gamze Karatas");
+            Ogrenci.List();
+        }
+    }
+}
